7bbb94203947e9b32550d0f47967c936096bb60d
Dominik Piatek <do.piatek@gmail.com>
2016-09-19 10:53:27 +0100

    Add bash snippets file

---
+++ bash.sh
@@ -0,0 +1,16 @@
+# Check for existence of enviroment variable. -z flag checks if string is equal
+# to 0
+if [ -z "$GH_TOKEN" ]; then
+  echo "GH_TOKEN not set. Generate one at https://github.com/settings/tokens"
+  exit 1
+fi
+
+# Check for existence of an executable, using type
+# -t gives only the type. The fn will give us 0 or 1 exit code
+exists() { type -t "$1" > /dev/null 2>&1; }
+
+if ! exists jq; then
+  echo "jq json parser not found; install with brew install jq"
+  exit 1
+fi
+
